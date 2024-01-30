366241cfb667e99a2c4dcb387687e4425f1d43c1
durga-ganabathy <56518655+durga-ganabathy@users.noreply.github.com>
2024-01-29 22:43:15 -0500

    api_keys.py

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "76376c840aa08f56eb522cc0e9c74246"
+weather_api_key = "YOUR KEY"
 
 # Geoapify API Key
-geoapify_key = "08b1eea8909045a2b2f96fc64e96a463"
+geoapify_key = "YOUR KEY"
